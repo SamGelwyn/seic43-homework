80bbb03c7b95de30fda5e5ab134ddf07dcc1fc8d
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-06 09:15:01 +1000

    throttled image search

--- Sam Gelwyn/week_07/Wednesday/image-search/js/search.js
+++ Sam Gelwyn/week_07/Wednesday/image-search/js/search.js
@@ -1,4 +1,9 @@
 const searchFlickr = function (keywords) {
+
+  if (state.lastPageReached) {
+    return; // Get out.
+  }
+
   console.log('searching for', keywords);
 
   const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?';
@@ -6,10 +11,22 @@ const searchFlickr = function (keywords) {
     method: 'flickr.photos.search', // not to be confused with HTTP methods
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: keywords,
-    format: 'json'
+    format: 'json',
+    page: state.nextPage++
   }).done(showImages).done(function (info) {
+    if (info.photos.page >= info.photos.pages) {
+      state.lastPageReached = true;
+    }
     console.log(info);
+    console.log(state);
   });
+};
+
+const throttledSearchFlickr = _.throttle(searchFlickr,1000, {trailing: false})
+
+const state = {
+  nextPage: 1,
+  lastPageReached: false
 };
 
 const showImages = function (results) {
@@ -40,6 +57,7 @@ $(document).ready(function () {
   $('#search').on('submit', function (event) {
     event.preventDefault();
 
+    state.nextPage = 1;
     $('#images').empty();
 
     const searchTerms = $('#query').val();
@@ -47,13 +65,16 @@ $(document).ready(function () {
   });
 
   $(window).on('scroll', function () {
+    // calculate scroll bottom
     const scrollBottom = $(document).height() - $(window).scrollTop() - $(window).height();
     console.log(scrollBottom);
 
+    // if the scroll bottom < 400px searchFlickr again
+    if (scrollBottom < 400) {
+      const searchTerms = $('#query').val();
+      throttledSearchFlickr(searchTerms); // page 1 again: HW: get the next page
+    }
 
-  })
-  // calculate scroll bottom
-  // if the scroll bottom < 200px
-    // searchFlickr again
-
+  });
+  
 });
